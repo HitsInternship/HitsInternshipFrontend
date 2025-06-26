97c4ac076ecef77496e385c54e82dbfb5a28e161
Карелина Елена <elena.karelina@gazprombank.ru>
2025-06-27 03:02:27 +0700

    сообщение при ошибки удаления позиции

--- src/entities/Position/hooks/useDeletePosition.ts
+++ src/entities/Position/hooks/useDeletePosition.ts
@@ -1,7 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
+import { AxiosError } from 'axios';
 
 import { deletePositions } from '../api/deletePositions';
+
+interface ErrorResponse {
+  message: string;
+}
 
 export const useDeletePosition = () => {
   const queryClient = useQueryClient();
@@ -12,8 +17,20 @@ export const useDeletePosition = () => {
       queryClient.invalidateQueries({ queryKey: ['positions'] });
       toast.success('Позиция удалена');
     },
-    onError: () => {
-      toast.error('Произошла ошибка!');
+    onError: (error) => {
+      const axiosError = error as AxiosError<ErrorResponse>;
+      console.log(axiosError?.response?.data?.message);
+
+      if (
+        axiosError.response &&
+        axiosError.response.data &&
+        axiosError.response.data.message ===
+          'You cannot delete this position, because there are vacancies with this position.'
+      ) {
+        toast.error('На основе этой позиции есть вакансии');
+      } else {
+        toast.error('Произошла ошибка');
+      }
     },
   });
 };
